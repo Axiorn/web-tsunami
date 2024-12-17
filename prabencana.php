a8db9decbdc2c1f79cb3dc32b459e7e6a228687d
latief55 <latief0505050@gmail.com>
2024-12-17 13:33:14 +0700

    test commit

--- prabencana.php
+++ prabencana.php
@@ -111,7 +111,7 @@
                         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
                             <i class="fa fa-bars"></i>
                         </button>
-                        <a class="navbar-brand" href="index">Tsunamiii<span>Center</span></a>
+                        <a class="navbar-brand" href="index">Tsunami<span>Center</span></a>
                     </div>
 
                     <!-- Navbar Menu -->
